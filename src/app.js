de456822e89223fe8d700e789be215a5275d5b33
Isabel Valdes <isabel.valdes@improving.com>
2022-06-06 17:28:40 -0500

    build: get activities

---
+++ src/app.js
@@ -0,0 +1,22 @@
+async function generalFetch({path, method, body}) {
+    const res = await fetch('http://localhost:6500/' + path, {
+        method,
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "text/html",
+            "Accept": "application/json"
+        }
+    })
+    
+    return res.json();
+}
+
+export async function getElements () {
+    const res = await generalFetch({
+        path: 'nova-api/activities',
+        method: 'GET'
+    });
+    
+    return res;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,110 +1,40 @@
 import { Observable } from "windowed-observable";
-const target = document.getElementById("rootReact");
-const renderer = import("modal/Renderer");
+import { getElements } from "./app";
+/* const target = document.getElementById("rootReact");
+const renderer = import("modal/Renderer"); */
 const bootstrap = import("calendar/Bootstrap");
-const vanillaButton = import("button/Button");
-const pomodoro = import("pomodoro/Pomodoro");
+/* const vanillaButton = import("button/Button");
+const pomodoro = import("pomodoro/Pomodoro"); */
 const calendarObservable = new Observable('calendar-date');
 
 
-renderer.then((res) => res.renderInVanilla(target));
+// peticion y guardado en localstorage
+
+getElements().then(res => {
+  console.log(res)
+  window.localStorage.setItem('activities', JSON.stringify(res))
+});
+
+/* renderer.then((res) => res.renderInVanilla(target));
 vanillaButton.then((res) => {
   const { button } = res.buttonF();
   document.getElementById("rootVanilla").append(button);
-});
-
-
+}); */
+let info = []
 bootstrap
-  .then((res) => {
-    
+  .then(async (res) => {
     const { calendar, button } = res.component();
     const target2 = document.getElementById("rootAngular");
-    calendar.srcData = [
-      {
-        AccountName: 'Itexico',
-        ActivityDate: '2022-05-06T09:35:04',
-        ActivityID: 0,
-        CategoryName: 'Available',
-        Comments: 'string',
-        EmployeeID: 0,
-        ProjectColor: 'blue',
-        ProjectID: 0,
-        ProjectName: 'Delivery',
-        StepID: 0,
-        Task: 'esto es un ticket',
-        TypeID: 0,
-        value: 5,
-        activeInProject: false,
-      },
-      {
-        AccountName: 'Itexico',
-        ActivityDate: '2022-05-06T09:35:04',
-        ActivityID: 0,
-        CategoryName: 'Available',
-        Comments: 'string',
-        EmployeeID: 0,
-        ProjectColor: 'blue',
-        ProjectID: 0,
-        ProjectName: 'Delivery',
-        StepID: 0,
-        Task: 'esto es un ticket',
-        TypeID: 0,
-        value: 5,
-        activeInProject: false,
-      },
-      {
-        AccountName: 'Itexico',
-        ActivityDate: '2022-05-06T09:35:04',
-        ActivityID: 0,
-        CategoryName: 'Available',
-        Comments: 'string',
-        EmployeeID: 0,
-        ProjectColor: 'blue',
-        ProjectID: 0,
-        ProjectName: 'Delivery',
-        StepID: 0,
-        Task: 'esto es un ticket',
-        TypeID: 0,
-        value: 5,
-        activeInProject: false,
-      },
-      {
-        AccountName: 'Itexico',
-        ActivityDate: '2022-05-06T09:35:04',
-        ActivityID: 0,
-        CategoryName: 'Available',
-        Comments: 'string',
-        EmployeeID: 0,
-        ProjectColor: 'blue',
-        ProjectID: 0,
-        ProjectName: 'Delivery',
-        StepID: 0,
-        Task: 'esto es un ticket',
-        TypeID: 0,
-        value: 5,
-        activeInProject: false,
-      },
-      {
-        AccountName: 'Itexico',
-        ActivityDate: '2022-05-06T09:35:04',
-        ActivityID: 0,
-        CategoryName: 'Available',
-        Comments: 'string',
-        EmployeeID: 0,
-        ProjectColor: 'blue',
-        ProjectID: 0,
-        ProjectName: 'Delivery',
-        StepID: 0,
-        Task: 'esto es un ticket',
-        TypeID: 0,
-        value: 5,
-        activeInProject: false,
-      },
-    ]
+    
     calendar.currentDate = new Date();
     calendarObservable.subscribe((date) => {
       calendar.currentDate = date;
       console.log(date, calendar.currentDate);
+    });
+    window.addEventListener('storage', () => {
+      info.concat(JSON.parse(window.localStorage.getItem('activities')));
+      calendar.srcData = info;
+      console.log(info)
     });
     button.style.position = 'fixed';
     button.style.bottom = '80px';
@@ -112,7 +42,8 @@ bootstrap
     target2.append(calendar, button)
   })
   .catch((err) => console.log(err.message));
-pomodoro.then(res=>{
+
+/* pomodoro.then(res=>{
  console.log(res)
 res.mount('#rootVue')
-})
+}) */
