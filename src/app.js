0db22848965854ed62a0c65f85950e9cec54ca59
Emma Aguila <Emma.Ramirez@improving.com>
2022-06-08 16:58:16 -0500

    fix: send data to calendar

--- src/app.js
+++ src/app.js
@@ -1,22 +1,19 @@
-async function generalFetch({path, method, body}) {
-    const res = await fetch('http://localhost:6500/' + path, {
-        method,
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "text/html",
-            "Accept": "application/json"
-        }
-    })
-    
-    return res.json();
+export async function generalFetch({ path, method/* , body */ }) {
+  const res = await fetch("http://localhost:6500/" + path, {
+    method,
+    // body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+//   if (!body) delete res.body
+  return res.json();
 }
 
-export async function getElements () {
-    const res = await generalFetch({
-        path: 'nova-api/activities',
-        method: 'GET'
-    });
-    
-    return res;
-}
-
+/* export async function getElements() {
+  const res = await generalFetch({
+    path: "nova-api/activities",
+    method: "GET",
+  });
+  return res;
+} */
--- src/index.js
+++ src/index.js
@@ -1,45 +1,48 @@
 import { Observable } from "windowed-observable";
-import { getElements } from "./app";
-/* const target = document.getElementById("rootReact");
-const renderer = import("modal/Renderer"); */
+const { generalFetch } = require("./app");
+const target = document.getElementById("rootReact");
+const renderer = import("modal/Renderer");
 const bootstrap = import("calendar/Bootstrap");
 /* const vanillaButton = import("button/Button");
 const pomodoro = import("pomodoro/Pomodoro"); */
-const calendarObservable = new Observable('calendar-date');
-
+const calendarObservable = new Observable("calendar-date");
 
 // peticion y guardado en localstorage
+const data = async()=>{
+  const fData = await generalFetch({ path: "nova-api/activities", method: "GET" })
+  return fData
+};
 
-getElements().then(res => {
-  console.log(res)
-  window.localStorage.setItem('activities', JSON.stringify(res))
-});
-
-/* renderer.then((res) => res.renderInVanilla(target));
-vanillaButton.then((res) => {
+renderer.then((res) => res.renderInVanilla(target));
+/* vanillaButton.then((res) => {
   const { button } = res.buttonF();
   document.getElementById("rootVanilla").append(button);
 }); */
-let info = []
+
 bootstrap
   .then(async (res) => {
     const { calendar, button } = res.component();
     const target2 = document.getElementById("rootAngular");
-    
+
+    /* // peticion y guardado en localstorage
+    generalFetch({ path: "nova-api/activities", method: "GET" })
+      .then((res) => {
+        window.localStorage.setItem("activities", JSON.stringify(res));
+        calendar.srcData = JSON.parse(
+          window.localStorage.getItem("activities")
+          );
+        })
+        .catch((err) => console.log(err)); */
+
+    calendar.srcData = await data();
     calendar.currentDate = new Date();
     calendarObservable.subscribe((date) => {
       calendar.currentDate = date;
-      console.log(date, calendar.currentDate);
     });
-    window.addEventListener('storage', () => {
-      info.concat(JSON.parse(window.localStorage.getItem('activities')));
-      calendar.srcData = info;
-      console.log(info)
-    });
-    button.style.position = 'fixed';
-    button.style.bottom = '80px';
-    button.style.right = '100px';
-    target2.append(calendar, button)
+    button.style.position = "fixed";
+    button.style.bottom = "80px";
+    button.style.right = "100px";
+    target2.append(calendar, button);
   })
   .catch((err) => console.log(err.message));
 
